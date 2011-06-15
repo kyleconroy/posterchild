abe7370a5bc2e012c6a9ba5556185a38638a8bd6
Kyle Conroy <kyle.j.conroy@gmail.com>
2011-06-15 12:20:24 -0700

    Added pretty print support for json and xml

--- js/churl.js
+++ js/churl.js
@@ -2,6 +2,37 @@
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
 
+  function formatXml(xml) {
+    var formatted = '';
+    var reg = /(>)(<)(\/*)/g;
+    xml = xml.replace(reg, '$1\r\n$2$3');
+    var pad = 0;
+    jQuery.each(xml.split('\r\n'), function(index, node) {
+        var indent = 0;
+        if (node.match( /.+<\/\w[^>]*>$/ )) {
+            indent = 0;
+        } else if (node.match( /^<\/\w/ )) {
+            if (pad != 0) {
+                pad -= 1;
+            }
+        } else if (node.match( /^<\w[^>]*[^\/]>.*$/ )) {
+            indent = 1;
+        } else {
+            indent = 0;
+        }
+
+        var padding = '';
+        for (var i = 0; i < pad; i++) {
+            padding += '  ';
+        }
+
+        formatted += padding + node + '\r\n';
+        pad += indent;
+    });
+
+    return formatted;
+  }
+
   var Hurl = Backbone.Model.extend({
 
     send: function() {
@@ -10,10 +41,21 @@ $(function(){
       $.ajax({
 	url: model.get("url"),
 	type: model.get("type"),
+	dataType: "text",
 	success: function(data, textStatus, jqXHR) {
+	  var contentType = jqXHR.getResponseHeader("Content-Type");
+
+	  if (contentType.indexOf("application/xml") !== -1) {
+	    var content = formatXml(data);
+	  } else if (contentType.indexOf("application/json") !== -1) {
+	    var content = js_beautify(data);
+	  } else {
+	    var content = data;
+	  }
+
 	  model.set({
 	    response: {
-	      content: data,
+	      content: content,
 	      headers: jqXHR.getAllResponseHeaders()
 	    },
 	    request: {
